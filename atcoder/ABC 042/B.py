55c06b23870e8ea99a47e0fa7b078b2e5c57f73b
mikanman <74596108+mikanman@users.noreply.github.com>
2021-03-21 20:32:42 -0900

    Delete atcoder directory

--- atcoder/ABC 042/B.py
+++
@@ -1,9 +0,0 @@
-n,l=map(int,input().split())
-ss=[]
-
-for i in range(n):
-  ss.append(input())
-
-ss.sort()
-
-print("".join(ss))
--- atcoder/ABC 043/A.py
+++
@@ -1,7 +0,0 @@
-n=int(input())
-t=0
-
-for i in range(n):
-  t=t+i+1
-
-print(t)
